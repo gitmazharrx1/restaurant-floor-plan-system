5b3d7a38d0cb195a36845376dcbd298dd0192665
web-mazhar <mazharulislam.enlightsolutions@gmail.com>
2025-02-24 15:21:23 +0600

    Resizer applied

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,47 @@ import { DropZoneDirective } from '../directive/DropZone.directive';
 })
 export class AppComponent {
   layout: any[] = [];
-
+  isResizing = false;
+  resizeIndex = -1;
+  startX = 0;
+  startY = 0;
+  startWidth = 0;
+  startHeight = 0;
   ngOnInit() {
     this.loadLayout();
   }
+  startResizing(event: MouseEvent, index: number) {
+    event.stopPropagation();
+    this.isResizing = true;
+    this.resizeIndex = index;
 
+    this.startX = event.clientX;
+    this.startY = event.clientY;
+    this.startWidth = this.layout[index].width || 50;
+    this.startHeight = this.layout[index].height || 50;
+
+    document.addEventListener('mousemove', this.resizeElement);
+    document.addEventListener('mouseup', this.stopResizing);
+  }
+
+  resizeElement = (event: MouseEvent) => {
+    if (!this.isResizing) return;
+
+    const dx = event.clientX - this.startX;
+    const dy = event.clientY - this.startY;
+    const newWidth = Math.max(30, this.startWidth + dx);
+    const newHeight = Math.max(30, this.startHeight + dy);
+
+    this.layout[this.resizeIndex].width = newWidth;
+    this.layout[this.resizeIndex].height = newHeight;
+  };
+
+  stopResizing = () => {
+    this.isResizing = false;
+    this.resizeIndex = -1;
+    document.removeEventListener('mousemove', this.resizeElement);
+    document.removeEventListener('mouseup', this.stopResizing);
+  };
   dragStart(event: DragEvent, type: string) {
     event.dataTransfer?.setData('text/plain', JSON.stringify({ id: this.generateId(), type }));
   }
@@ -25,10 +61,13 @@ export class AppComponent {
     this.layout.push(event);
   }
 
-  updateItemPosition(event: { index: number, x: number, y: number }) {
-    this.layout[event.index].x = event.x;
-    this.layout[event.index].y = event.y;
+  updateItemPosition(index: number, event: { x: number; y: number }) {
+    if (!this.layout[index]) return;
+
+    this.layout[index].x = event.x;
+    this.layout[index].y = event.y;
   }
+
 
   deleteItem(index: number) {
     this.layout.splice(index, 1);
